1618e0bf541b636ef2b3be435165acced5250cb2
pranjalr004 <pranjalr376@gmail.com>
2026-06-08 18:47:59 +0530

    Installing Required Dependencies

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import Expense from './pages/Dashboard/Expense'
+
 const App = () => {
   return (
-    <div className=''>
-      App
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>}/>
+          <Route path="/login" exact element={<Login/>}/>
+          <Route path="/signup" exact element={<Signup/>}/>
+          <Route path="/dashboard" exact element={<Home />}/>
+          <Route path="/income" exact element={<Income />}/>
+          <Route path="/expense" exact element={<Expense />}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
